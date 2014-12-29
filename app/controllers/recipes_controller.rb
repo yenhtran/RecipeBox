2d80166a40527bd82c688bbaea257d933dd8071d
Yen Tran <yentran@Yens-MacBook-Pro-4.local>
2014-12-28 18:24:25 -0800

    add strong params for new recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,18 @@ class RecipesController < ApplicationController
 	end
 
 	def new
+		@recipe = Recipe.new
 	end
 
 	def create
+		@recipe = Recipe.new(recipe_params)
 	end
 
 	private
+
+	def recipe_params
+		params.require(:recipe).permit(:title, :description)
+	end
 
 	def find_recipe
 		@recipe = Recipe.find(params[:id])
